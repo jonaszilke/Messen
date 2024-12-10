96be7f7660128ea47bcd3e95901573f625285ba8
Jonas Zilke <jonas.zilke@uni-bielefeld.de>
2024-12-10 08:19:17 +0100

    remove todo

--- 2024/10_October/11_frankfurter_buchmesse.py
+++ 2024/10_October/11_frankfurter_buchmesse.py
@@ -36,7 +36,7 @@ def parse_exhibitor(ex: Exhibitor):
     css_street = '#__next > div.MuiBox-root.css-g9qx4c > main > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.css-obamm2 > div > div > div:nth-child(1) > div > div.MuiBox-root.css-1at62qq > p:nth-child(1)'
     css_city = '#__next > div.MuiBox-root.css-g9qx4c > main > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.css-obamm2 > div > div > div:nth-child(1) > div > div.MuiBox-root.css-1at62qq > p:nth-child(2)'
     css_country = '#__next > div.MuiBox-root.css-g9qx4c > main > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.css-obamm2 > div > div > div:nth-child(1) > div > div.MuiBox-root.css-1at62qq > p:nth-child(3)'
-    css_url = '#__next > div.MuiBox-root.css-g9qx4c > main > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.css-obamm2 > div > div > div:nth-child(3) > div > div > div.MuiBox-root.css-19ambhl > a > h6'  # TODO
+    css_url = '#__next > div.MuiBox-root.css-g9qx4c > main > div > div > div > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.MuiGrid-grid-md-6.MuiGrid-grid-lg-4.css-obamm2 > div > div > div:nth-child(3) > div > div > div.MuiBox-root.css-19ambhl > a > h6'
 
     ex.name = tools.get_information_from_css_link(css_name, throw_exception=True)
     ex.url = tools.get_information_from_css_link(css_url, throw_exception=False, timeout=0.5)
